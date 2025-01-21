eee017f6dc800ab1b2150d65f019d16ba5383362
Seth Curry <seth@scurry.io>
2025-01-21 10:55:18 -0600

    Adding an index of containers

--- bin/chemgr/chemgr.go
+++ bin/chemgr/chemgr.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"github.com/SethCurry/chemhacktica-infra/internal/docker"
 	"github.com/alecthomas/kong"
-	"github.com/docker/docker/client"
 	"go.uber.org/zap"
 )
 
 type (
-	HealthCheckCLI struct{}
-	CLI            struct {
+	CLI struct {
 		HealthCheck HealthCheckCLI `cmd:"healthcheck" help:"Check the health of the ChemHacktica services"`
 	}
 
@@ -17,32 +14,6 @@ type (
 		Logger *zap.Logger
 	}
 )
-
-// Run is the entrypoint for the HealthCheckCLI command.
-// It is called as health-check on the CLI.
-func (h *HealthCheckCLI) Run(bound *Bound) error {
-	logger := bound.Logger
-	logger.Debug("executing HealthCheckCLI")
-
-	apiClient, err := client.NewClientWithOpts(client.FromEnv)
-	if err != nil {
-		return err
-	}
-	defer apiClient.Close()
-
-	results, err := docker.CheckContainers(apiClient)
-	if err != nil {
-		return err
-	}
-
-	for _, result := range results {
-		if !result.Success {
-			logger.Info(result.Message, zap.String("subject", result.Subject))
-		}
-	}
-
-	return nil
-}
 
 func main() {
 	cfg := zap.NewProductionConfig()
