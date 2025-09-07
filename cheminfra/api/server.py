2d7adf36bb15527e0035559b3585b7df062f9dfd
Seth Curry <seth@scurry.io>
2025-09-07 16:18:58 -0400

    returning last 100 most recent statuses

--- cheminfra/api/server.py
+++ cheminfra/api/server.py
@@ -29,14 +29,34 @@ async def get_session():
 
 SessionDep = Annotated[Session, Depends(get_session)]
 
+
 @app.get("/api/v1/docker/containers")
 def get_containers(session: SessionDep):
     containers = session.query(Container).all()
     return containers
 
+
+@app.get("/api/v1/docker/containers/{container_id}/status")
+def get_container_status(container_id: int, session: SessionDep):
+    statuses = (
+        session.query(ContainerStatus)
+        .filter(ContainerStatus.container_id == container_id)
+        .order_by(ContainerStatus.recorded_at.desc())
+        .limit(100)
+        .all()
+    )
+    return statuses
+
+
 @app.get("/api/v1/docker/containers/{container_id}")
 def get_container(container_id: int, session: SessionDep):
-    container = session.query(Container).filter(Container.id == container_id).join(ContainerStatus).options(joinedload(Container.statuses)).one_or_none()
+    container = (
+        session.query(Container)
+        .filter(Container.id == container_id)
+        .join(ContainerStatus)
+        .options(joinedload(Container.statuses))
+        .one_or_none()
+    )
     return container
 
 
@@ -48,7 +68,7 @@ def healthcheck():
 @app.get("/api/v1/rpc/restart")
 def make_restart():
     try:
-      subprocess.run(["make", "restart"], cwd=config.deployment.core_dir, check=True)
-      return {"message": "Restarted successfully"}
+        subprocess.run(["make", "restart"], cwd=config.deployment.core_dir, check=True)
+        return {"message": "Restarted successfully"}
     except subprocess.CalledProcessError as e:
-      return {"message": "Failed to restart", "error": str(e)}
+        return {"message": "Failed to restart", "error": str(e)}
